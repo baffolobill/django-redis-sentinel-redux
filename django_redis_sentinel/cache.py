8b99d6a3b136c8881163948194664998f634d0f3
baffolobill <baffolobill@yandex.ru>
2018-08-05 19:46:59 +0300

    all this stuff inited in parent class

--- django_redis_sentinel/cache.py
+++ django_redis_sentinel/cache.py
@@ -1,4 +1,4 @@
-from django_redis.cache import RedisCache, DJANGO_REDIS_IGNORE_EXCEPTIONS
+from django_redis.cache import RedisCache
 from django_redis.util import load_class
 
 
@@ -7,13 +7,10 @@ class RedisSentinelCache(RedisCache):
     Forces SentinelClient instead of DefaultClient
     """
     def __init__(self, server, params):
-        super(RedisCache, self).__init__(params)
-        self._server = server
-        self._params = params
+        super().__init__(server, params)
 
         options = params.get("OPTIONS", {})
-        self._client_cls = options.get("CLIENT_CLASS", "django_redis_sentinel.client.SentinelClient")
+        self._client_cls = options.get("CLIENT_CLASS", 
+                                       "django_redis_sentinel.client.SentinelClient")
         self._client_cls = load_class(self._client_cls)
         self._client = None
-
-        self._ignore_exceptions = options.get("IGNORE_EXCEPTIONS", DJANGO_REDIS_IGNORE_EXCEPTIONS)
